f096dcfe069d8d2be1bccec17d9e45435fd3b90a
Alex Windle <alexwindle85@gmail.com>
2017-02-06 16:10:37 -0500

    Basic component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Regex Exercise</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, enter a search term and text field.
         </p>
+        <form id="form">
+          <div>Search term: <input type="text"></input></div>
+          <div>Text field: <textarea rows="5" cols="35"></textarea></div>
+          <div><button type="submit">Submit</button></div>
+          <div><button type="reset">Clear</button></div>
+        </form>
       </div>
     );
   }
