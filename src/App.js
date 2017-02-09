ade57162d9b6c2e2831f384c5ff50ffd2f42929f
Alex Windle <alexwindle85@gmail.com>
2017-02-09 18:07:05 -0500

    Submit button fires action. Data non-persistent.

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ class App extends Component {
     super(props); 
     this.state = {input: '', textarea: ''}; 
 
-    this.inputChange = this.inputChange.bind(this); //need to understand
+    this.inputChange = this.inputChange.bind(this);
     this.textareaChange = this.textareaChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  clickTest() {
-    alert("You clicked the button!"); 
-  }
-  
   inputChange(e){ 
     this.setState({input: e.target.value}); 
   } 
 
   textareaChange(e){
     this.setState({textarea: e.target.value}); 
+  }
+
+  handleSubmit(){
+    console.log("Input: ", this.state.input);  
+    console.log("Textarea: ", this.state.textarea); 
+    alert("click"); 
   }
 
   render() {
@@ -34,10 +37,33 @@ class App extends Component {
           To get started, enter a search term and text field.
         </p>
         <form id="form">
-          <div><input type="text" value={this.state.input} onChange={this.inputChange} placeholder="Enter search term..."/></div>
-          <div><textarea rows="5" cols="35"  value={this.state.textarea} onChange={this.textareaChange} placeholder="Text field..." ></textarea></div>
-          <div><button onClick={this.clickTest}>Submit</button></div>
-          <div><button type="reset">Clear</button></div>
+          <div>
+            <input 
+              type="text" 
+              value={this.state.input} 
+              onChange={this.inputChange} 
+              placeholder="Enter search term..."
+            />
+          </div>
+          <div>
+            <textarea 
+              rows="5" 
+              cols="35" 
+              value={this.state.textarea} 
+              onChange={this.textareaChange} 
+              placeholder="Text field..." >
+            </textarea>
+          </div>
+          <div>
+            <button onClick={this.handleSubmit}>
+              Submit
+            </button>
+          </div>
+          <div>
+            <button type="reset">
+              Clear
+            </button>
+          </div>
         </form>
       </div>
     );
