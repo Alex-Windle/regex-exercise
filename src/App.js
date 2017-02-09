9e17f8735af7a31329b838943b3e3880a547a03d
Alex Windle <alexwindle85@gmail.com>
2017-02-09 17:02:33 -0500

    Bind inputChange function to change handler.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ class App extends Component {
   constructor(props){
     super(props); 
     this.state = {input: '', textarea: ''}; 
+
+    this.inputChange = this.inputChange.bind(this); //need to understand
   }
 
-  render() {
+  clickTest() {
+    alert("You clicked the button!"); 
+  }
+  
+  inputChange(e){ 
+    this.setState({input: e.target.value}); 
+  } 
 
-    function clickTest() {
-      alert("You clicked the button!"); 
-    }; 
+  render() {
 
     return (
       <div className="App">
@@ -23,9 +29,9 @@ class App extends Component {
           To get started, enter a search term and text field.
         </p>
         <form id="form">
-          <div><input type="text" value={this.state.input} placeholder="Enter search term..."/></div>
+          <div><input type="text" value={this.state.input} onChange={this.inputChange} placeholder="Enter search term..."/></div>
           <div><textarea rows="5" cols="35"  value={this.state.textarea} placeholder="Text field..." ></textarea></div>
-          <div><button onClick={clickTest}>Submit</button></div>
+          <div><button onClick={this.clickTest}>Submit</button></div>
           <div><button type="reset">Clear</button></div>
         </form>
       </div>
